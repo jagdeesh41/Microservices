f8fbfc34a03254fc9726529191cfff297e6c1100
jagdeesh41 <pepjaggu@gmail.com>
2024-08-12 00:18:30 +0530

    added config server

---
+++ cloud-config-server/src/test/java/kom/learn/cloud_config_server/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kom.learn.cloud_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
